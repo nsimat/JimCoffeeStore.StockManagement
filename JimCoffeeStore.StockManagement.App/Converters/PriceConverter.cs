aee75aa4ccece4841062650aeb85678d5e42179d
Michel Nsimba <nsimba_matondo@hotmail.com>
2021-03-15 22:23:38 +0100

    Adding the price converter, PriceConverter, to convert price to euro currency.

---
+++ JimCoffeeStore.StockManagement.App/Converters/PriceConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace JimCoffeeStore.StockManagement.App.Converters
+{
+    public class PriceConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value.ToString() + " €";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
